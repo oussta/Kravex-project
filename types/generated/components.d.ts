d9c5e221e0258bc11865e6ae344f1dea24d87407
oussta <ousstatada@gmail.com>
2026-02-17 03:29:48 +0100

    Initial commit: Strapi backend setup

---
+++ types/generated/components.d.ts
@@ -0,0 +1,21 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+  };
+  attributes: {
+    keywords: Schema.Attribute.String;
+    meta_description: Schema.Attribute.Text;
+    meta_title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.seo': SharedSeo;
+    }
+  }
+}
